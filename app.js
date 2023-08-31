573b386e44a2c392ed900e38568c2da1f326a5e1
vikrant <notvikrant80@gmail.com>
2023-08-31 08:46:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
+require("dotenv").config;
+
+const app = express();
+
+app.get("/", async (req, res, next) => {
+  res.send("hello");
+});
+
+app.use(async (req, res, next) => {
+  next(createError.NotFound("This route does not exists"));
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
