3bce3a081d4792043c3460fa47a980746eadb8a5
vikrant <notvikrant80@gmail.com>
2023-09-04 08:21:26 +0530

    add: token verification

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const createError = require("http-errors");
 require("dotenv").config;
 require("./helpers/init_mongodb.js");
+const { verifyAccessToken } = require("./helpers/jwt_helper");
 
 const AuthRoute = require("./routes/auth.route");
 
@@ -12,7 +13,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", async (req, res, next) => {
+app.get("/", verifyAccessToken, async (req, res, next) => {
   res.send("hello");
 });
 
--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+const JWT = require("jsonwebtoken");
 const createError = require("http-errors");
 
 module.exports = {
@@ -7,16 +7,29 @@ module.exports = {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "1h",
+        expiresIn: "15s",
         audience: userId,
       };
-      jwt.sign(payload, secret, options, (error, token) => {
+      JWT.sign(payload, secret, options, (error, token) => {
         if (error) {
           console.log(error);
           reject(createError.InternalServerError());
         }
         resolve(token);
       });
+    });
+  },
+  verifyAccessToken: (req, res, next) => {
+    if (!req.headers["authorization"]) return next(createError.Unauthorized());
+    const authHeader = req.headers["authorization"];
+    const bearerToken = authHeader.split(" ");
+    const token = bearerToken[1];
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) {
+        return next(createError.Unauthorized);
+      }
+      req.payload = payload;
+      next();
     });
   },
 };
